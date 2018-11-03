5d98f309b80ab961e5fc3b576e4c9834d5c775e7
peter-filina <peter.filina@hotmail.com>
2018-11-03 08:55:16 +0100

    added slf4j logging framework with slf4j logger

--- src/test/java/org/pfi/NewTest.java
+++ src/test/java/org/pfi/NewTest.java
@@ -1,11 +1,18 @@
 package org.pfi;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class NewTest {
-  @Test
-  public void f() {
-	  Assert.assertEquals(1, 1);
-  }
+
+	final Logger logger = LoggerFactory.getLogger(NewTest.class);
+
+	@Test
+	public void f() {
+		logger.info("test");
+		Assert.assertEquals(1, 1);
+	}
 }
